65c22fd93bdd9e9236d97d7e667ffcf961523835
Dongho Kim <go49mog@mytum.de>
2025-05-08 16:50:34 +0200

    added

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+// eslint.config.js
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import jsConfig from "@eslint/js"; // Import CommonJS module as default
+
+const { configs } = jsConfig;
+
+export default defineConfig([
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.node,
+        ...globals.es2021,
+      },
+    },
+    ...configs.recommended,
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "warn",
+    },
+  },
+]);
