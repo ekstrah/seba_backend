5c35069e19e9a214dcfb78da240978a2d08347c5
Dongho Kim <dongho@ekstrah.com>
2025-06-14 08:36:43 +0200

    changelog

--- models/consumer.model.js
+++ models/consumer.model.js
@@ -2,28 +2,16 @@ import mongoose from "mongoose";
 import { User } from "./user.model.js";
 import { Address } from "./address.model.js";
 import { PaymentMethod } from "./paymentMethod.model.js";
+import { Cart } from "./cart.model.js";
 
 // Consumer-specific schema that extends User
 const consumerSchema = new mongoose.Schema({
     // Inherits all User fields (email, password, name, phone, etc.)
     // Additional Consumer-specific fields
-    cart: [{
-        product: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Product',
-            required: true
-        },
-        quantity: {
-            type: Number,
-            required: true,
-            min: 1,
-            default: 1
-        },
-        addedAt: {
-            type: Date,
-            default: Date.now
-        }
-    }],
+    activeCart: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Cart'
+    },
     deliveryAddresses: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Address'
@@ -32,10 +20,6 @@ const consumerSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: 'PaymentMethod'
     }],
-    // orderHistory: [{
-    //     type: mongoose.Schema.Types.ObjectId,
-    //     ref: 'Order'
-    // }],
     // wishlist: [{
     //     type: mongoose.Schema.Types.ObjectId,
     //     ref: 'Product'
@@ -58,14 +42,40 @@ const consumerSchema = new mongoose.Schema({
     discriminatorKey: 'role'  // This will be set to 'consumer' for all Consumer documents
 });
 
+// Helper method to get or create active cart
+consumerSchema.methods.getActiveCart = async function () {
+    if (!this.activeCart) {
+        const cart = await Cart.create({
+            consumer: this._id,
+            status: 'active'
+        });
+        this.activeCart = cart._id;
+        await this.save();
+        return cart;
+    }
+
+    const cart = await Cart.findById(this.activeCart);
+    if (!cart || cart.status !== 'active') {
+        const newCart = await Cart.create({
+            consumer: this._id,
+            status: 'active'
+        });
+        this.activeCart = newCart._id;
+        await this.save();
+        return newCart;
+    }
+
+    return cart;
+};
+
 // Helper methods for address management
-consumerSchema.methods.addDeliveryAddress = async function(addressData) {
+consumerSchema.methods.addDeliveryAddress = async function (addressData) {
     const address = await Address.create(addressData);
     this.deliveryAddresses.push(address._id);
     return this.save();
 };
 
-consumerSchema.methods.removeDeliveryAddress = async function(addressId) {
+consumerSchema.methods.removeDeliveryAddress = async function (addressId) {
     this.deliveryAddresses = this.deliveryAddresses.filter(
         addr => addr.toString() !== addressId.toString()
     );
@@ -73,27 +83,27 @@ consumerSchema.methods.removeDeliveryAddress = async function(addressId) {
     return this.save();
 };
 
-consumerSchema.methods.getDefaultAddress = async function() {
+consumerSchema.methods.getDefaultAddress = async function () {
     return await Address.findOne({
         _id: { $in: this.deliveryAddresses },
         isDefault: true
     });
 };
 
-consumerSchema.methods.setDefaultAddress = async function(addressId) {
+consumerSchema.methods.setDefaultAddress = async function (addressId) {
     // First, unset all default addresses
     await Address.updateMany(
         { _id: { $in: this.deliveryAddresses } },
         { isDefault: false }
     );
-    
+
     // Then set the new default
     await Address.findByIdAndUpdate(addressId, { isDefault: true });
     return this.save();
 };
 
 // Helper methods for payment method management
-consumerSchema.methods.addPaymentMethod = async function(paymentMethodData) {
+consumerSchema.methods.addPaymentMethod = async function (paymentMethodData) {
     const paymentMethod = await PaymentMethod.create({
         ...paymentMethodData,
         consumer: this._id
@@ -102,7 +112,7 @@ consumerSchema.methods.addPaymentMethod = async function(paymentMethodData) {
     return this.save();
 };
 
-consumerSchema.methods.removePaymentMethod = async function(paymentMethodId) {
+consumerSchema.methods.removePaymentMethod = async function (paymentMethodId) {
     this.paymentMethods = this.paymentMethods.filter(
         pm => pm.toString() !== paymentMethodId.toString()
     );
@@ -110,7 +120,7 @@ consumerSchema.methods.removePaymentMethod = async function(paymentMethodId) {
     return this.save();
 };
 
-consumerSchema.methods.getDefaultPaymentMethod = async function() {
+consumerSchema.methods.getDefaultPaymentMethod = async function () {
     return await PaymentMethod.findOne({
         _id: { $in: this.paymentMethods },
         isDefault: true,
@@ -118,15 +128,15 @@ consumerSchema.methods.getDefaultPaymentMethod = async function() {
     });
 };
 
-consumerSchema.methods.setDefaultPaymentMethod = async function(paymentMethodId) {
+consumerSchema.methods.setDefaultPaymentMethod = async function (paymentMethodId) {
     await PaymentMethod.updateMany(
-        { 
+        {
             _id: { $in: this.paymentMethods },
-            isDefault: true 
+            isDefault: true
         },
         { isDefault: false }
     );
-    
+
     await PaymentMethod.findByIdAndUpdate(paymentMethodId, { isDefault: true });
     return this.save();
 };
